6b9171eb5980771448f08253dd4be0d1263dcdfc
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-08 22:29:50 +0100

    Refactor

--- js/main.js
+++ js/main.js
@@ -9,29 +9,20 @@ function randomMove() {
   return [randomNumber(), randomNumber(), randomNumber()]
 }
 
-function getNewSolution(sudoku, restrictions) {
-  for (let i = 0; i < MAX_MOVES; ++i) {
-    const newMove = randomMove();
-    const newSolution = sudoku.concat([newMove])
-    let valid = true;
-    for (const restriction of restrictions) {
-      if (!restriction(newSolution)) {
-        valid = false;
-        break;
-      }
-    }
-    if (valid) {
-      return newSolution;
+function isValid(restrictions, newSolution) {
+  for (const restriction of restrictions) {
+    if (!restriction(newSolution)) {
+      return false;
     }
   }
-  return null;
+  return true;
 }
 
 function solve(restrictions, sudoku) {
   for (let i=0; i<MAX_MOVES; ++i) {
-    let newSolution = getNewSolution(sudoku, restrictions);
-    if (newSolution == null) {
-      return null;
+    const newSolution = sudoku.concat([randomMove()]);
+    if (!isValid(restrictions, newSolution)) {
+      continue;
     }
 
     if (newSolution.length === 81) {
@@ -46,43 +37,43 @@ function solve(restrictions, sudoku) {
   return null;
 }
 
-function sudokuRestriction(sudoku) {
-  const positions = {};
-  const rows = new Set();
-  const columns = new Set();
-  const boxes = new Set();
+function getPositionKey(move) {
+  return move.slice(0, 2).toString();
+}
 
-  for (const move of sudoku) {
-    const key = move.slice(0, 2).toString();
-    if (key in positions) {
-      return false;
+function getRowKey(move) {
+  return [move[1], move[2]].toString();
+}
+
+function getColumnKey(move) {
+  return [move[0], move[2]].toString();
+}
+
+function getBoxKey(move) {
+  const box = Math.floor(move[0] / 3) + 3 * (Math.floor(move[1] / 3));
+  return [box, move[2]].toString();
+}
+
+function sudokuRestrictions() {
+  return [sudokuRestriction(getPositionKey), sudokuRestriction(getRowKey),
+    sudokuRestriction(getColumnKey), sudokuRestriction(getBoxKey)];
+}
+
+function sudokuRestriction(keyFunction) {
+  function restriction(sudoku) {
+    const keys = new Set();
+
+    for (const move of sudoku) {
+      const key = keyFunction(move);
+      if (keys.has(key)) {
+        return false;
+      }
+      keys.add(key);
     }
 
-    const value = move[2];
-
-    const row = move[1];
-    const rowKey = [row, value].toString();
-    if (rows.has(rowKey)) {
-      return false;
-    }
-    rows.add(rowKey);
-
-    const column = move[0];
-    const columnKey = [column, value].toString();
-    if (columns.has(columnKey)) {
-      return false;
-    }
-    columns.add(columnKey);
-
-    const box = Math.floor(column/3) + 3*(Math.floor(row/3));
-    const boxKey = [box, value].toString();
-    if (boxes.has(boxKey)) {
-      return false;
-    }
-    boxes.add(boxKey);
+    console.log("Keys", keys, sudoku);
+    return true;
   }
-  if (sudoku.length > 53) {
-    console.log("Got valid", sudoku, positions, rows, columns, boxes);
-  }
-  return true;
+
+  return restriction;
 }
