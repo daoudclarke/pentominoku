d579f509c951fbfa47e501bcebb3a66833346dc4
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-08 20:55:52 +0100

    Sudoku restriction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-const MAX_MOVES = 10;
+const MAX_MOVES = 3;
 
 function randomNumber() {
   return Math.ceil(Math.random() * 9);
@@ -28,11 +28,61 @@ function getNewSolution(sudoku, restrictions) {
 }
 
 function solve(restrictions, sudoku) {
-  let newSolution = getNewSolution(sudoku, restrictions);
+  for (let i=0; i<MAX_MOVES; ++i) {
+    let newSolution = getNewSolution(sudoku, restrictions);
+    if (newSolution == null) {
+      return null;
+    }
 
-  if (newSolution.length === 81) {
-    return newSolution;
+    if (newSolution.length === 81) {
+      return newSolution;
+    }
+
+    let solution = solve(restrictions, newSolution);
+    if (solution != null) {
+      return solution;
+    }
   }
+  return null;
+}
 
-  return solve(restrictions, newSolution);
+function sudokuRestriction(sudoku) {
+  const positions = {};
+  const rows = new Set();
+  const columns = new Set();
+  const boxes = new Set();
+
+  for (const move of sudoku) {
+    const key = move.slice(0, 2).toString();
+    if (key in positions) {
+      return false;
+    }
+
+    const value = move[2];
+
+    const row = move[1];
+    const rowKey = [row, value].toString();
+    if (rows.has(rowKey)) {
+      return false;
+    }
+    rows.add(rowKey);
+
+    const column = move[0];
+    const columnKey = [column, value].toString();
+    if (columns.has(columnKey)) {
+      return false;
+    }
+    columns.add(columnKey);
+
+    const box = Math.floor(column/3) + 3*(Math.floor(row/3));
+    const boxKey = [box, value].toString();
+    if (boxes.has(boxKey)) {
+      return false;
+    }
+    boxes.add(boxKey);
+  }
+  if (sudoku.length > 53) {
+    console.log("Got valid", sudoku, positions, rows, columns, boxes);
+  }
+  return true;
 }
