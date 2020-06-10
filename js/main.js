8800c607c181770f38b750b9c58ba44b14824c6d
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-10 22:07:53 +0100

    Improve search

--- js/main.js
+++ js/main.js
@@ -12,21 +12,20 @@ for (let i=1; i<=9; ++i) {
   }
 }
 
+let singlePossibilities = new Map();
+singlePossibilities.set(1, 1);
+singlePossibilities.set(2, 2);
+singlePossibilities.set(4, 3);
+singlePossibilities.set(8, 4);
+singlePossibilities.set(16, 5);
+singlePossibilities.set(32, 6);
+singlePossibilities.set(64, 7);
+singlePossibilities.set(128, 8);
+singlePossibilities.set(256, 9);
+
 function choose(choices) {
   let index = Math.floor(Math.random() * choices.length);
   return choices[index];
-}
-
-function randomMove(sudoku) {
-  const positions = new Set();
-  for (const move of sudoku) {
-    positions.add([move[0], move[1]]);
-  }
-
-  const validPositions = new Set([...allPositions].filter(x => !positions.has(x)));
-
-  const position = choose(Array.from(validPositions));
-  return [position[0], position[1], randomNumber()]
 }
 
 
@@ -37,15 +36,95 @@ for (let i=0; i<81; ++i) {
 }
 
 
-function getPossible(restrictions, solution) {
+let nearlySolved = new Set();
+for (let i=0; i<9; ++i) {
+  for (let j=i+1; j<9; ++j) {
+    for (let k=j+1; k<9; ++k) {
+      const value = (1 << i) | (1 << j) | (i << k);
+      nearlySolved.add(value);
+    }
+  }
+}
+console.log("Nearly solved", nearlySolved);
+
+
+function binaryToArray(binary) {
+  const result = [];
+  for (let i=0; i<9; ++i) {
+    if ((binary & 1) === 1) {
+      result.push(i + 1);
+    }
+    binary = binary >> 1;
+  }
+  return result;
+}
+
+
+function getPossible(restrictions, solution, previousPossible, maxDepth) {
   const possible = allPossible.slice();
+
   for (const restriction of restrictions) {
     const restrictionPossible = restriction(solution);
     for (let i=0; i<possible.length; ++i) {
       possible[i] &= restrictionPossible[i];
     }
+    if (maxDepth === 3) {
+      console.log("Restrict");
+      printPossible(possible);
+    }
   }
+  if (maxDepth === 3) {
+    console.log("After initial restriction");
+    printPossible(possible);
+  }
+
+  if (maxDepth === 0) {
+    return possible;
+  }
+
+  const newSolution = solution.slice();
+  while (true) {
+    const restriction = searchRestriction(restrictions, newSolution, possible, previousPossible, maxDepth);
+    if (restriction == null) {
+      break;
+    }
+    if (maxDepth === 3) {
+      console.log("Found restriction", maxDepth, restriction);
+    }
+    const [i, decimal] = restriction;
+    possible[i] &= ~(1 << (decimal - 1));
+    if (singlePossibilities.has(possible[i])) {
+      const possibility = singlePossibilities.get(possible[i]);
+      if (maxDepth === 3) {
+        console.log("Found single possibility", maxDepth, i, possibility);
+      }
+      newSolution.push([i, possibility]);
+    }
+  }
+
   return possible;
+}
+
+
+function searchRestriction(restrictions, solution, possible, previousPossible, maxDepth) {
+    for (let i=0; i<possible.length; ++i) {
+    if (nearlySolved.has(possible[i]) && previousPossible[i] !== possible[i]) {
+      let decimals = binaryToArray(possible[i]);
+      for (const decimal of decimals) {
+        const newSolution = solution.slice();
+        // console.log("Searching with additional possibility", maxDepth, i, decimal)
+        newSolution.push([i, decimal]);
+        const newPossible = getPossible(restrictions, newSolution, possible, maxDepth - 1);
+        const minimumValue = Math.min(...newPossible);
+        if (minimumValue === 0) {
+          // We can rule out this decimal from the actual solution
+          return [i, decimal];
+        }
+      }
+    }
+  }
+  // console.log("Found no restriction", maxDepth);
+  return null;
 }
 
 
@@ -92,7 +171,7 @@ function boxRestriction(fixedPoints) {
     const row = (i - column)/9;
     const firstRow = row - (row % 3);
     const firstColumn = column - (column % 3);
-    console.log("First row column", row, column, firstRow, firstColumn);
+    // console.log("First row column", row, column, firstRow, firstColumn);
     let valueBinary = 1 << (value - 1);
     const notValue = allNumbers - valueBinary;
     for (let k=firstRow; k<firstRow + 3; ++k) {
@@ -134,14 +213,24 @@ function testBox() {
 
 function test() {
   let restrictions = [rowRestriction, columnRestriction, boxRestriction];
-  let fixedPoints = [[33,5]];
-  let possible = getPossible(restrictions, fixedPoints);
+  // let fixedPoints = [[33,5]];
+  let fixedPoints = [[0, 5], [1, 3], [4, 7],
+    [9, 6], [12, 1], [13, 9], [14, 5],
+    [19, 9], [20, 8], [25, 6],
+    [27, 8], [31, 6], [35, 3],
+    [36, 4], [39, 8], [41, 3], [44, 1],
+    [45, 7], [49, 2], [53, 6],
+    [55, 6], [60, 2], [61, 8],
+    [66, 4], [67, 1], [68, 9], [71, 5],
+    [76, 8], [79, 7], [80, 9]
+  ]
+  let possible = getPossible(restrictions, fixedPoints, allPossible.slice(), 3);
   printPossible(possible);
 }
 
 function printPossible(possible) {
   console.log("Possible");
   for (let i=0; i<81; i+=9) {
-    console.log(possible.slice(i, i+9));
+    console.log(possible.slice(i, i+9).map(x => binaryToArray(x).toString()));
   }
 }
