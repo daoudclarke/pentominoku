d7b063cd7f05922bcfb540f106c0761f5b835ea4
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-12 22:52:03 +0100

    Missing import

--- js/possible.test.js
+++ js/possible.test.js
@@ -4,6 +4,7 @@
 // allPossible = require('./possible');
 
 import {allPossible} from './possible';
+import {expect} from "@jest/globals";
 
 test('all possible has the right number', () => {
   expect(allPossible.length === 81);
