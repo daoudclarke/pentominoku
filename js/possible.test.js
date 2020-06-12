fe805775b29a164e730216a2ddfbdbae0336764f
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-12 22:50:36 +0100

    Get yarn tests working again

--- js/possible.test.js
+++ js/possible.test.js
@@ -1,4 +1,9 @@
-const allPossible = require('./possible');
+// const {allNumbers, allPossible} = require('./possible');
+
+// import {allPossible} from "./possible";
+// allPossible = require('./possible');
+
+import {allPossible} from './possible';
 
 test('all possible has the right number', () => {
   expect(allPossible.length === 81);
