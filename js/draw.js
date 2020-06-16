019cf49a1f708591b344c62f8379154d0b18235c
Daoud Clarke <daoud.clarke@gmail.com>
2020-06-16 22:23:48 +0100

    Nest the square

--- js/draw.js
+++ js/draw.js
@@ -8,11 +8,18 @@ export function drawSudoku() {
     for (let j=0; j<9; ++j) {
       let x = 20 + 90*i;
       let y = 20 + 90*j;
-      const rect = draw.rect(90, 90)
-        .attr({x: x, y: y, fill: '#fff', stroke: '#000', 'stroke-width': 1});
-      const text = draw.text('1').attr(
+
+      const square = draw.nested();
+      square.attr({x: x, y: y});
+
+      const rect = square.rect(90, 90).attr({x: 0, y: 0, fill: '#fff', stroke: '#000', 'stroke-width': 1});
+      const text = square.text('1').attr(
         {'font-size': '60px'});
-      text.center(x + 45, y + 45);
+      text.center(45, 45);
+
+      square.mouseover(() => rect.attr({fill: '#eee'}));
+      square.mouseout(() => rect.attr({fill: '#fff'}));
+
     }
   }
 }
